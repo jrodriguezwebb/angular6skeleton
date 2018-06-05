376776d1f7319bc7650998406939eba2c10f9fc4
Jesús Rodríguez <jesus.jarp@gmail.com>
2018-06-05 16:37:41 +0200

    Mock Server

---
+++ angular/mock/config.js
@@ -0,0 +1,23 @@
+module.exports = function () {
+    return {
+        port: 3000,
+        browserSyncEnabled: true,
+        // restRootFolder: "/home/everis/sites/everis-web-eforms/web-eforms-ui/src/mock",
+        //restRootFolder: "./src/mock",
+        restRootFolder: "",
+        restPrefix: "api",
+        restServiceCallDelay: 0,
+        routerUrlPrefix: "/screen",
+        mimeTypes: {
+            "html": "text/html",
+            "jpeg": "image/jpeg",
+            "jpg": "image/jpeg",
+            "png": "image/png",
+            "js": "text/javascript",
+            "css": "text/css",
+            "json": "application/json",
+            "svg": "image/svg+xml",
+            "manifest": "text/cache-manifest"
+        }
+    }
+};
